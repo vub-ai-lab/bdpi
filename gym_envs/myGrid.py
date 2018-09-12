cf2d6bb93dc232e4e5a99623e48cd32e43465b7d
Denis Steckelmacher <steckdenis@yahoo.fr>
2018-09-12 12:36:22 +0200

    Add license headers

--- gym_envs/myGrid.py
+++ gym_envs/myGrid.py
@@ -1,3 +1,21 @@
+# This file is part of Bootstrapped Dual Policy Iteration
+# 
+# Copyright 2018, Vrije Universiteit Brussel (http://vub.ac.be)
+#     authored by Hélène Plisnier <helene.plisnier@vub.be>
+#
+# BDPI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# BDPI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BDPI.  If not, see <http://www.gnu.org/licenses/>.
+
 import gym
 
 from gym import spaces
